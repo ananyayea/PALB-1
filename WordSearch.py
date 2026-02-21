38ff8f575898bf4c7f150ea1e261d627926c7310
Ananya <ananya2610goel@gmail.com>
2026-02-21 09:31:42 +0530

    Create WordSearch.py

---
+++ WordSearch.py
@@ -0,0 +1,37 @@
+class Solution:
+    def exist(self, board, word):
+        rows, cols = len(board), len(board[0])
+        
+        def dfs(r, c, index):
+            # If all characters matched
+            if index == len(word):
+                return True
+            
+            # Boundary + mismatch check
+            if (r < 0 or c < 0 or 
+                r >= rows or c >= cols or 
+                board[r][c] != word[index]):
+                return False
+            
+            # Mark as visited
+            temp = board[r][c]
+            board[r][c] = "#"
+            
+            # Explore 4 directions
+            found = (dfs(r+1, c, index+1) or
+                     dfs(r-1, c, index+1) or
+                     dfs(r, c+1, index+1) or
+                     dfs(r, c-1, index+1))
+            
+            # Restore cell (backtrack)
+            board[r][c] = temp
+            
+            return found
+        
+        # Try every cell as starting point
+        for i in range(rows):
+            for j in range(cols):
+                if dfs(i, j, 0):
+                    return True
+        
+        return False
